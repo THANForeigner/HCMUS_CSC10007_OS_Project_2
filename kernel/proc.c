4a62990dc029e9a88e73bb4ded74e0bcc0982dde
fazeisfaze <phucbin0903@gmail.com>
2026-04-19 14:51:39 +0700

    oke

--- kernel/proc.c
+++ kernel/proc.c
@@ -666,7 +666,9 @@ either_copyin(void *dst, int user_src, uint64 src, uint64 len)
 
 int pgaccess(uint64 start_address, int page_number, uint64 mask)
 {
-    uint64 mask_buffer  = 0;
+  if (page_number > 64)
+    return -1;
+  uint64 mask_buffer  = 0;
     pte_t * pte;
     for (int i = 0; i < page_number; i++){
       pte = walk(myproc()->pagetable, (start_address+PGSIZE*i), 0);
